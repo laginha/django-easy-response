311820fadacffd9111761e1b478f99809cabeba7
Diogo Machado <diogo.laginha.machado@gmail.com>
2015-07-29 17:55:33 +0100

    update version number (1.0.1)

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ reqs = [str(each.req) for each in install_reqs if each.req]
 
 setup(
     name             = 'django-easy-response',
-    version          = '1.0.0',
+    version          = '1.0.1',
     author           = "Diogo Laginha",
     author_email     = "diogo.laginha.machado@gmail.com",
     url              = 'https://github.com/laginha/django-easy-response',
